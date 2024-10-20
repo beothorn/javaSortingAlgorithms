96719120ceb564126b6e99572bab56180770e6f3
Beothorn <lucass@gmail.com>
2024-10-20 21:22:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sorting algorithms
+
+This is an example app, mainly used to test javaflame.  
+
+Algorithms:  
+- Bubble sort
+- Quicksort inplace
+- Insertion sort
+- Merge sort
---
+++ src/main/java/com/github/beothorn/tests/TestFiltersC.java
@@ -0,0 +1,21 @@
+package com.github.beothorn.tests;
+
+public class TestFiltersC{
+
+    public TestFiltersC(String arg){
+        System.out.println(arg);
+    }
+
+    public String functionABC(){
+        return "ABC";
+    }
+
+    public String functionDEF(){
+        return "DEF";
+    }
+
+    @Override
+    public String toString() {
+        return "TestC";
+    }
+}
